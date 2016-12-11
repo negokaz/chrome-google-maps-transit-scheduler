8b3a7577357c1533a39cf4572feef809b5a642af
negokaz <negokaz@gmail.com>
2016-12-11 23:38:22 +0900

    Changed permissions to allow all google domains

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.match(new RegExp('^https://www.google.co.jp/maps/dir/.+/am=t/.+')) && changeInfo.status === "complete") {
+  if (tab.url.match(new RegExp('^https://www.google.[^/]+/maps/dir/.+/am=t/.+')) && changeInfo.status === "complete") {
     chrome.tabs.executeScript(tabId, {file: 'bower_components/jquery/dist/jquery.js'});
     chrome.tabs.executeScript(tabId, {file: 'js/route_exporter.js'});
     chrome.tabs.insertCSS(tabId, {file: 'style/route_exporter.css'});
