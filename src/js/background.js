d1a28ac89407d4aecd65266326d54fa5446d7c70
negokaz <negokaz@gmail.com>
2016-12-11 23:49:37 +0900

    Changed background.js to load js as right order

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,8 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   if (tab.url.match(new RegExp('^https://www.google.[^/]+/maps/dir/.+/am=t/.+')) && changeInfo.status === "complete") {
-    chrome.tabs.executeScript(tabId, {file: 'bower_components/jquery/dist/jquery.js'});
-    chrome.tabs.executeScript(tabId, {file: 'js/route_exporter.js'});
+    chrome.tabs.executeScript(tabId, {file: 'bower_components/jquery/dist/jquery.js'}, function() {
+      chrome.tabs.executeScript(tabId, {file: 'js/route_exporter.js'});
+    });
     chrome.tabs.insertCSS(tabId, {file: 'style/route_exporter.css'});
   }
 });
