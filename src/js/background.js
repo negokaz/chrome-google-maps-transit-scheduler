bc9f1c6e603fbbffb79451bcaae9d81a3394f5a6
negokaz <negokaz@gmail.com>
2016-12-18 13:27:27 +0900

    Fixed error

--- src/js/background.js
+++ src/js/background.js
@@ -1,5 +1,7 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-  if (tab.url.match(new RegExp('^https://www.google.[^/]+/maps/dir/.+/am=t/.+')) && changeInfo.status === "complete") {
+  if (tab.url
+        && tab.url.match(new RegExp('^https://www.google.[^/]+/maps/dir/.+/am=t/.+'))
+        && changeInfo.status === "complete") {
     chrome.tabs.executeScript(tabId, {file: 'bower_components/jquery/dist/jquery.js'}, function() {
       chrome.tabs.executeScript(tabId, {file: 'js/route_exporter.js'});
     });
